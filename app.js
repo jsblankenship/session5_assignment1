de247324926962987d22f64bcb68859284f4fd7f
Jeff Blankenship <jsblankenship44@gmail.com>
2017-04-03 14:47:49 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+
+
+		var  name = "Jeff Blankenship";
+		var  age  = 42;
+		var  dob  = "March 25, 1975";
+		var  pob  = "Charleston WV"; 
+		alert("The name is " +name  + " age is " +age + " born on " +dob + " in " +pob);
